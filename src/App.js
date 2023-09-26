2506c8b50c9964e29ab7b32bd3ad038afa6ac162
Guda Pavaneeshwar Reddy <92384421+PavaneeshwarReddy@users.noreply.github.com>
2023-09-26 13:01:06 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Navbar from "./Pages/Navbar";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
